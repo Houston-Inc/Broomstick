56e03381f88cca98191992d68e74d6822860cb1a
Tarmo Aidantausta <tarmo.aidantausta@houston-inc.com>
2013-03-22 09:01:03 +0200

    Fixed the http server

--- http-server.js
+++ http-server.js
@@ -5,29 +5,32 @@ var stat = require('node-static'),
 
 
 httpProxy.createServer(function (req, res, proxy) {
-    proxy.proxyRequest(req, res, {
-        host: '127.0.0.1',
-        port: 8088
-    });
+    if(req.url.indexOf('/rest') !== -1 || req.url.indexOf('spring') !== -1 || req.url == '/user') {
+        console.log('proxy to: 127.0.0.1:8089', req.url);
+        proxy.proxyRequest(req, res, {
+            host: '127.0.0.1',
+            port: 8089
+        });
+    } else {
+        console.log('proxy to: 127.0.0.1:8088', req.url);
+        proxy.proxyRequest(req, res, {
+            host: '127.0.0.1',
+            port: 8088
+        });
+    }
 }).listen(8080);
 
 http.createServer(function (request, response) {
-    if(request.method === "GET") {
+    if(request.method === 'GET') {
         request.addListener('end', function () {
             fileServer.serve(request, response, function (e, res) {
-                if(request.url.indexOf('/error/401') !== -1) {
-                    response.writeHead(401);
-                    response.end();
-                } else if (request.url.indexOf('/error/500') !== -1) {
-                    response.writeHead(500);
-                    response.end();
-                } else if (e && (e.status === 404)) { // If the file wasn't found
+                if (e && (e.status === 404)) { // If the file wasn't found
                     fileServer.serveFile('/index.html', 200, {}, request, response);
                 }
             });
         });
     }
-
+    request.resume();
 }).listen(8088);
 
 var testFileServer = new stat.Server('./');
@@ -36,13 +39,13 @@ require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         testFileServer.serve(request, response);
     });
+    request.resume();
 }).listen(7357);
 
 
 var release = new stat.Server('./release');
 
 require('http').createServer(function (request, response) {
-
     if(request.method === "GET") {
         request.addListener('end', function () {
             release.serve(request, response, function (e, res) {
@@ -52,7 +55,7 @@ require('http').createServer(function (request, response) {
             });
         });
     }
-
+    request.resume();
 }).listen(8090);
 
 var testBuild = new stat.Server('./test-build');
@@ -61,4 +64,5 @@ require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         testBuild.serve(request, response);
     });
+    request.resume();
 }).listen(7358);
